b29c020e248fd9ce98a0546618ce78fb481250a6
onlyj <onlyj@jisay>
2023-02-15 10:15:38 +0900

    0215

--- src/main/java/com/fullstack/controller/MemberController.java
+++ src/main/java/com/fullstack/controller/MemberController.java
@@ -23,10 +23,18 @@ public class MemberController extends HttpServlet {
     	String pw = request.getParameter("pw");
     	String name = request.getParameter("name");
     	
-    	MVCMemberDTO dto = null;
+    	MVCMemberDTO dto = new MVCMemberDTO();
     	dto.setId(id);
     	dto.setName(name);
     	dto.setPw(pw);
+    	
+    	//위 DTO가 Setting되었으나 DAO를 호출하여 DTO를 DB에 Insert합니다
+    	//참고로 num필드는 기존 시퀀스값을 이용예정이니 시퀀스 있는지 확인하세요
+    	//없으시면 만드시고 있으시면 currval값 찍어보세요
+    	
+    	
+    	
+    	
     }
 
 }
